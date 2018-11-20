717859896bd888b719bb9814fdbf74c4f1f75aec
Andrius30 <andrius.rom@gmail.com>
2018-11-20 19:37:37 +0200

    Hangman Update

--- functions.js
+++ functions.js
@@ -3,8 +3,15 @@ var words = [
     'medis',
     'stalas',
     'kate',
-    'the war on drugs',
-    'everyone who knew'
+    'thewarondrugs',
+    'everyonewhoknew',
+    'television',
+    'bridge',
+    'table',
+    'roof',
+    'spiderman',
+    'batman',
+    'league'
 ];
 var hidenArray = [];
 var randomNumber = Math.floor(Math.random() * words.length);
@@ -16,9 +23,16 @@ var l;
 var lifes = 8;
 var gussedLetter = 0;
 var points = 0;
+const input = document.querySelector('#gues');
+const regex = /^[a-zA-Z\s]*$/;
+const btn = document.getElementById('btn');
+const btnWin = document.getElementById('btnwin');
+console.log(btn);
 
-// show word on console
+// You Lost Game Over
 console.log(randomWord);
+
+
 
 // hide Word
 var hideWord = function() {
@@ -34,75 +48,87 @@ var hideWord = function() {
     $("#randWord").text(s);
 }
 
-var newFunction = function() {
-    var variable = $('#gues').val();
-    var letter = $('#alert').text(variable);
-    if(variable != ' '){
-    	var isItAGoodGuess = false;
-        for (var j = 0; j < randomWord.length; j++) {
-            if (randomWord[j].toLowerCase() === variable.toLowerCase()) {
-            	 points = points + 100;
-                hidenArray[j] = variable;
-                isItAGoodGuess = true;
-                gussedLetter++;
+var variable = $('#gues').val();
+var letter = $('#alert').text(variable);
 
-            }
-		  }
-        }
-        for(var i = 0; i < randomWord.length; i++){
-        if(gussedLetter >= randomWord.length){
-        	// console.log("You WON " + randomWord);
-        	document.getElementById("win").innerHTML = randomWord;
-        	 points = (points * 2);
-                
+input.addEventListener('keyup', function() {
+    var isItAGoodGuess = false;
+    let char = input.value;
+    if (char.match(regex)) {
+        let contains = randomWord.includes(char);
+        if (contains && !guessedLetters.includes(char)) {
+            for (let i = 0; i < randomWord.length; i++) {
+                if (randomWord[i] === char) {
+                    isItAGoodGuess = true;
+                    hidenArray[i] = char;
+                    gussedLetter++;
+                    points += 100;
                 }
             }
+        }
+    } else {
+        alert('bad input');
+    }
+    input.value = '';
 
-            // if letter guessed already
-            for(var i = 0; i < guessedLetters.length; i++){
-            	if(guessedLetters[i] == variable){
-            		console.log("Tokia raide jau buvo!");
-            		points = (points - 10);
-            	}
-            }
-        // if its not a good guess lifes geting one less
-        if (!isItAGoodGuess) {
-        	if (lifes > 0) {
-            lifes--;
-            points = (points - 50);
-			$('#lifes').text(lifes);
-		}else{
-			console.log("GAME OVER!! Correct Answer Was: " + randomWord);
-			// alert("Press Enter To Start New Game");
-			points = points / 2;
-		}
-		  document.getElementById('points').innerHTML = points;
-	}
+    if (gussedLetter >= randomWord.length) {
+        // You WON reset
+        btnWin.addEventListener('click', function() {
+            lifes = 8;
+            gussedLetter = 0;
+            guessedLetters = [];
+            hidenArray = [];
+            randomNumber = Math.floor(Math.random() * words.length);
+            randomWord = words[randomNumber];
+            hideWord();
+        });
+        $(function() {
+            $('#win').arcticmodal();
+            $('secretWord').text(randomWord);
+        });
+        points = (points * 2);
+    }
+
+    // if letter guessed already
+    if (!guessedLetters.includes(char)) {
         //previous guesses
         g = guessedLetters.join(",");
         $("#guesses").text(g);
-        guessedLetters.push(variable)
-        // update hiden word
-        s = hidenArray.join(" ");
-        $("#randWord").text(s);
-}
+        guessedLetters.push(char);
+    } else {
+        isItAGoodGuess = true;
+        alert("Tokia raide jau buvo!" + char);
+        points = (points - 10);
+    }
 
+    // if its not a good guess lifes geting one less
+    if (!isItAGoodGuess) {
+        if (lifes > 0) {
+            lifes--;
+            points = (points - 50);
+            $('#lifes').text(lifes);
+        } else {
+            btn.addEventListener('click', function() {
+                lifes = 8;
+                gussedLetter = 0;
+                guessedLetters = [];
+                hidenArray = [];
+                randomNumber = Math.floor(Math.random() * words.length);
+                randomWord = words[randomNumber];
+                hideWord();
+            });
+            $(function() {
+                $('#exampleModal').arcticmodal();
+            });
+            // new GameOver reset
+
+            points = points / 2;
+        }
+        document.getElementById('points').innerHTML = points;
+    }
+    // update hiden word
+    s = hidenArray.join(" ");
+    $("#randWord").text(s);
+});
 // padaryti kad galima butu kartoti zaidima
-
- // console.log(points);
- 
-// input from keyboard
- $("#gues").keydown(function(event) {
- 	//updating user input
-
- 	newFunction();
-// on enter dont let refresh page
-  event=event ||window.event;
-  var key = event.keyCode;
-  if(key==13) {
-  	// alert("Persikraunam!!");
-     return true; 
-  }
-    });
-
 $('#randWord').text(hideWord());
