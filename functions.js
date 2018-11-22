4c7e1c7ad7d634ba510eb4125aa5dd6438560195
Andrius30 <andrius.rom@gmail.com>
2018-11-22 15:54:08 +0200

    hangman update

--- functions.js
+++ functions.js
@@ -18,7 +18,7 @@ var randomNumber = Math.floor(Math.random() * words.length);
 var randomWord = words[randomNumber];
 var guessedLetters = [];
 var s;
-var lifes = 8;
+var lifes = 7;
 var gussedLetter = 0;
 var points = 0;
 const input = document.querySelector('#gues');
@@ -27,6 +27,7 @@ const btn = document.getElementById('btn');
 const btnwin = document.getElementById('btnwin');
 const gameOverSound = new sound('sounds/gameOver.mp3');
 const bgSound = new sound('sounds/backgroundS.mp3');
+
 
 // You Lost Game Over
 console.log(randomWord);
@@ -77,7 +78,7 @@ input.addEventListener('keyup', function() {
     if (gussedLetter >= randomWord.length) {
         // You WON reset
         btnwin.addEventListener('click', function(event) {
-            lifes = 8;
+            lifes = 7;
             gussedLetter = 0;
             guessedLetters = [];
             hidenArray = [];
@@ -88,7 +89,6 @@ input.addEventListener('keyup', function() {
         $(function() {
             $('#win').arcticmodal();
         });
-        $('secretWord').text(randomWord);
         points = (points * 2);
     }
 
@@ -106,25 +106,54 @@ input.addEventListener('keyup', function() {
 
     // if its not a good guess lifes geting one less
     if (!isItAGoodGuess) {
-        if (lifes > 0) {
-            lifes--;
+        if (lifes != 0 && lifes > 0) {
+             lifes--;
             points = (points - 50);
             $('#lifes').text(lifes);
+
+            // hangman image sprites
+            var image = document.getElementById('div');
+            var image2 = document.getElementById('div');
+            document.body.appendChild(image);
+            switch (lifes) {
+                case 6:
+                  image.className = 'case6';
+                    break;
+                case 5:
+                   image.className = 'case5';
+                    break;
+                case 4:
+                    image.className = 'case4';
+                    break;
+                case 3:
+                    image.className = 'case3';
+                    break;
+                case 2:
+                    image.className = 'case2';
+                    break;
+                case 1:
+                    image.className = 'case1';
+                    break;
+                case 0:
+                   image.className = 'case0';
+                    break;
+            }
+
         } else {
             btn.addEventListener('click', function() {
                 gameOverSound.stop();
-                lifes = 8;
+                lifes = 7;
                 gussedLetter = 0;
                 guessedLetters = [];
                 hidenArray = [];
                 randomNumber = Math.floor(Math.random() * words.length);
                 randomWord = words[randomNumber];
                 hideWord();
-
             });
             $(function() {
                 $('#exampleModal').arcticmodal();
             });
+            $('#secretword').text(randomWord);
             // game over sound effects
             gameOverSound.play();
             points = points / 2;
@@ -135,6 +164,8 @@ input.addEventListener('keyup', function() {
     s = hidenArray.join(" ");
     $("#randWord").text(s);
 });
+
+
 $('#randWord').text(hideWord());
 var bg = function() {
     bgSound.play();
